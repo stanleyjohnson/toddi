08762d79f506e7adb369ea0fdca25a0c20baaa64
Stanley Johnson <stanley.johnson@rivigo.com>
2019-05-15 17:51:59 +0530

    added hot rel

--- src/index.js
+++ src/index.js
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {AppContainer} from 'react-hot-loader';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root');
+ 
+ReactDOM.render(
+    <AppContainer>
+        <App />
+    </AppContainer>,
+  rootEl
+);
+
+if (module.hot) {
+    module.hot.accept('./App', () => {
+        const NextApp = require('./App').default; // eslint-disable-line global-require
+        ReactDOM.render(
+            <AppContainer>
+                <NextApp />
+            </AppContainer>,
+            rootEl
+        );
+    });
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
